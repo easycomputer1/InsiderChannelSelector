20b17c5b4e368fac7eecd0026425bd6d027a07c8
ecomter <t14725803692@outlook.com>
2022-12-05 10:03:18 +0800

    basic

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -30,6 +30,7 @@ namespace InsiderChannelSelector
         }
 
         RegistryKey key = Registry.LocalMachine;
+        RegistryKey keyExa = Registry.CurrentUser;
         int build = Environment.OSVersion.Version.Build;
 
         private void Start(object sender, RoutedEventArgs e)
@@ -52,32 +53,39 @@ namespace InsiderChannelSelector
             {
                 none();
             }
-            
+            messageShow();
             
         }
        private void dev()
         {
-            setIns("dev");
-            key.OpenSubKey("SOFTWARE\\Microsoft\\WindowsSelfHost\\Applicability", true).SetValue("BranchName", "Dev", RegistryValueKind.String);
+            setIns("Dev", "Dev Channel",2);
+            Doing.IsActive = false;
+          
         }
         private void beta()
         {
-            setIns("beta");
+            setIns("Beta", "Beta Channel",4);
         }
         private void rp()
         {
-            setIns("release preview");
+            setIns("ReleasePreview", "Release Preview Channel",8);
         }
         private void none()
         {
 
         }
-        private void setIns(string channelSet)
+        private async void messageShow()
+        {
+            var msgS = new Windows.UI.Popups.MessageDialog("配置完成！请重启电脑") { Title = "notice!" };
+            await msgS.ShowAsync();
+        }
+        private void setIns(string channelSet,string Fancy,int BRL)
         {
             key.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\WindowsUpdate\\Orchestrator", true).SetValue("EnableUUPScan", "1", RegistryValueKind.DWord);
             key.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\WindowsUpdate\\SLS\\Programs\\Ring%Ring%", true).SetValue("Enabled", "1", RegistryValueKind.DWord);
             key.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\WindowsUpdate\\SLS\\Programs\\WUMUDCat", true).SetValue("WUMUDCATEnabled", "1", RegistryValueKind.DWord);
             key.OpenSubKey("SOFTWARE\\Microsoft\\WindowsSelfHost\\Applicability", true).SetValue("EnablePreviewBuilds", "2", RegistryValueKind.DWord);
+            key.OpenSubKey("SOFTWARE\\Microsoft\\WindowsSelfHost\\Applicability", true).SetValue("BranchName", channelSet, RegistryValueKind.String);
             key.OpenSubKey("SOFTWARE\\Microsoft\\WindowsSelfHost\\Applicability", true).SetValue("IsBuildFlightingEnabled", "1", RegistryValueKind.DWord);
             key.OpenSubKey("SOFTWARE\\Microsoft\\WindowsSelfHost\\Applicability", true).SetValue("IsConfigSettingsFlightingEnabled", "1", RegistryValueKind.DWord);
             key.OpenSubKey("SOFTWARE\\Microsoft\\WindowsSelfHost\\Applicability", true).SetValue("IsConfigExpFlightingEnabled", "0", RegistryValueKind.DWord);
@@ -108,6 +116,45 @@ namespace InsiderChannelSelector
             key.OpenSubKey("SOFTWARE\\Microsoft\\WindowsSelfHost\\UI\\Visibility", true).SetValue("UIServiceDrivenElementVisibility", "0", RegistryValueKind.DWord);
             key.OpenSubKey("SOFTWARE\\Microsoft\\WindowsSelfHost\\UI\\Visibility", true).SetValue("UIErrorMessageVisibility", "192", RegistryValueKind.DWord);
             key.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\DataCollection", true).SetValue("AllowTelemetry", "3", RegistryValueKind.DWord);
+            key.OpenSubKey("SOFTWARE\\Policies\\Microsoft\\Windows\\WindowsUpdate", true).SetValue("BranchReadinessLevel", BRL, RegistryValueKind.DWord);
+            key.OpenSubKey("SOFTWARE\\Microsoft\\WindowsSelfHost\\UI\\Visibility", true).SetValue("UIHiddenElements_Rejuv", "65534", RegistryValueKind.DWord);
+            key.OpenSubKey("SOFTWARE\\Microsoft\\WindowsSelfHost\\UI\\Visibility", true).SetValue("UIDisabledElements_Rejuv", "65535", RegistryValueKind.DWord);
+            key.OpenSubKey("SOFTWARE\\Microsoft\\WindowsSelfHost\\UI\\Selection", true).SetValue("UIRing", "External", RegistryValueKind.String);
+            key.OpenSubKey("SOFTWARE\\Microsoft\\WindowsSelfHost\\UI\\Selection", true).SetValue("UIContentType", "Mainline", RegistryValueKind.String);
+            key.OpenSubKey("SOFTWARE\\Microsoft\\WindowsSelfHost\\UI\\Selection", true).SetValue("UIBranch",Channel, RegistryValueKind.String);
+            key.OpenSubKey("SOFTWARE\\Microsoft\\WindowsSelfHost\\UI\\Selection", true).SetValue("UIOptin", "1", RegistryValueKind.DWord);
+            key.OpenSubKey("SOFTWARE\\Microsoft\\WindowsSelfHost\\Applicability", true).SetValue("RingBackup", "External", RegistryValueKind.String);
+            key.OpenSubKey("SOFTWARE\\Microsoft\\WindowsSelfHost\\Applicability", true).SetValue("RingBackupV2", "External", RegistryValueKind.String);
+            key.OpenSubKey("SOFTWARE\\Microsoft\\WindowsSelfHost\\Applicability", true).SetValue("BranchBackup", Channel, RegistryValueKind.String);
+            key.OpenSubKey("SOFTWARE\\Microsoft\\WindowsSelfHost\\Cache", true).SetValue("PropertyIgnoreList", "AccountsBlob;;CTACBlob;FlightIDBlob;ServiceDrivenActionResults", RegistryValueKind.String);
+            key.OpenSubKey("SOFTWARE\\Microsoft\\WindowsSelfHost\\Cache", true).SetValue("RequestedCTACAppIds", "WU;FSS", RegistryValueKind.String);
+            key.OpenSubKey("SOFTWARE\\Microsoft\\WindowsSelfHost\\Account", true).SetValue("SupportedTypes", "3", RegistryValueKind.DWord);
+            key.OpenSubKey("SOFTWARE\\Microsoft\\WindowsSelfHost\\Account", true).SetValue("Status", "8", RegistryValueKind.DWord);
+            key.OpenSubKey("SOFTWARE\\Microsoft\\WindowsSelfHost\\Applicability", true).SetValue("UseSettingsExperience", "0", RegistryValueKind.DWord);
+            key.OpenSubKey("SOFTWARE\\Microsoft\\WindowsSelfHost\\ClientState", true).SetValue("AllowFSSCommunications", "0", RegistryValueKind.DWord);
+            key.OpenSubKey("SOFTWARE\\Microsoft\\WindowsSelfHost\\ClientState", true).SetValue("UICapabilities", "1", RegistryValueKind.DWord);
+            key.OpenSubKey("SOFTWARE\\Microsoft\\WindowsSelfHost\\ClientState", true).SetValue("IgnoreConsolidation", "1", RegistryValueKind.DWord);
+            key.OpenSubKey("SOFTWARE\\Microsoft\\WindowsSelfHost\\ClientState", true).SetValue("MsaUserTicketHr", "0", RegistryValueKind.DWord);
+            key.OpenSubKey("SOFTWARE\\Microsoft\\WindowsSelfHost\\ClientState", true).SetValue("MsaDeviceTicketHr", "0", RegistryValueKind.DWord);
+            key.OpenSubKey("SOFTWARE\\Microsoft\\WindowsSelfHost\\ClientState", true).SetValue("SValidateOnlineHr", "0", RegistryValueKind.DWord);
+            key.OpenSubKey("SOFTWARE\\Microsoft\\WindowsSelfHost\\ClientState", true).SetValue("LastHR", "0", RegistryValueKind.DWord);
+            key.OpenSubKey("SOFTWARE\\Microsoft\\WindowsSelfHost\\ClientState", true).SetValue("ErrorState", "0", RegistryValueKind.DWord);
+            key.OpenSubKey("SOFTWARE\\Microsoft\\WindowsSelfHost\\ClientState", true).SetValue("PilotInfoRing", "3", RegistryValueKind.DWord);
+            key.OpenSubKey("SOFTWARE\\Microsoft\\WindowsSelfHost\\ClientState", true).SetValue("RegistryAllowlistVersion", "4", RegistryValueKind.DWord);
+            key.OpenSubKey("SOFTWARE\\Microsoft\\WindowsSelfHost\\ClientState", true).SetValue("FileAllowlistVersion", "1", RegistryValueKind.DWord);
+            key.OpenSubKey("SOFTWARE\\Microsoft\\WindowsSelfHost\\UI", true).SetValue("UIControllableState", "0", RegistryValueKind.DWord);
+            key.OpenSubKey("SOFTWARE\\Microsoft\\WindowsSelfHost\\UI\\Selection", true).SetValue("UIDialogConsent", "0", RegistryValueKind.DWord);
+            key.OpenSubKey("SOFTWARE\\Microsoft\\WindowsSelfHost\\UI\\Selection", true).SetValue("UIUsage", "26", RegistryValueKind.DWord);
+            key.OpenSubKey("SOFTWARE\\Microsoft\\WindowsSelfHost\\UI\\Selection", true).SetValue("OptOutState", "25", RegistryValueKind.DWord);
+            key.OpenSubKey("SOFTWARE\\Microsoft\\WindowsSelfHost\\UI\\Selection", true).SetValue("AdvancedToggleState", "24", RegistryValueKind.DWord);
+            key.OpenSubKey("SYSTEM\\Setup\\WindowsUpdate", true).SetValue("AllowWindowsUpdate", "1", RegistryValueKind.DWord);
+            key.OpenSubKey("SYSTEM\\Setup\\MoSetup", true).SetValue("AllowUpgradesWithUnsupportedTPMOrCPU", "1", RegistryValueKind.DWord);
+            key.OpenSubKey("SYSTEM\\Setup\\LabConfig", true).SetValue("BypassRAMCheck", "1", RegistryValueKind.DWord);
+            key.OpenSubKey("SYSTEM\\Setup\\LabConfig", true).SetValue("BypassSecureBootCheck", "1", RegistryValueKind.DWord);
+            key.OpenSubKey("SYSTEM\\Setup\\LabConfig", true).SetValue("BypassStorageCheck", "1", RegistryValueKind.DWord);
+            key.OpenSubKey("SYSTEM\\Setup\\LabConfig", true).SetValue("BypassTPMCheck", "1", RegistryValueKind.DWord);
+            Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\PCHC", true).SetValue("UpgradeEligibility", "1", RegistryValueKind.DWord);
+            
         }
 
     }
